15f92d410bf42a4d160dd7ffa45b47d1932d115c
SamBz26 <samuelbenitezmesa@gmail.com>
2025-10-31 11:10:15 -0500

    Proceso pendiente ejercicio 3

---
+++ ex_02/src/PrimComponente.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PrimComponente = () => {
+  return (
+    <div>Hola Mundo como Componente</div>
+  )
+}
---
+++ ex_03/src/ListadoApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { useState } from 'react'
+
+export const ListadoApp = () => {
+  return (
+    <div>ListadoApp</div>
+  )
+}
